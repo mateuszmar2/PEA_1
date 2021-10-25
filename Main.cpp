82b022e5bf3c03f7a8426002909c0d879d7dc0db
Mateusz Marciniec <mateuszmar2@gmail.com>
2021-10-25 11:43:18 +0200

    Add generating random data and BF

--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,7 @@
 #include "Towns.h"
 #include "Branch_and_Bound.h"
 #include "Brute_Force.h"
+#include "Dynamic_Programming.h"
 
 #include <iostream>
 #include <limits>
@@ -19,6 +20,7 @@ void menu()
              << "Which action you want to perform? Type appropriate number " << endl;
         cout << "p - Print data " << endl;
         cout << "l - Load data from file " << endl;
+        cout << "g - Generate random data" << endl;
         cout << "b - Branch and Bound algorithm " << endl;
         cout << "d - Dynamic programming algorithm " << endl;
         cout << "f - BruteForce algorithm " << endl;
@@ -41,6 +43,20 @@ void menu()
             cout << endl;
             towns.loadDataFromFile(filename);
             break;
+        case 'g':
+        {
+            int value = 0;
+            cout << "Enter how many towns you want to create: ";
+            cin >> value;
+            if (value <= 0)
+            {
+                cout << "Invalid number of towns" << endl;
+                break;
+            }
+            towns.generateRandomData(value);
+            towns.printData();
+            break;
+        }
         case 'b': // B&B
         {
             if (towns.getTowns().empty())
@@ -63,12 +79,26 @@ void menu()
             break;
         }
         case 'd': // DP
+        {
             if (towns.getTowns().empty())
             {
                 cout << "Load data first" << endl;
                 break;
             }
+            std::chrono::steady_clock::time_point start =
+                std::chrono::steady_clock::now();
+            DynamicProgramming dp(towns.getTowns());
+            dp.startDP();
+            std::chrono::steady_clock::time_point end =
+                std::chrono::steady_clock::now();
+            dp.printRoute();
+            std::cout << "Czas wykonania: "
+                      << std::chrono::duration_cast<std::chrono::microseconds>(
+                             end - start)
+                             .count()
+                      << "us\n";
             break;
+        }
         case 'f': // BF
         {
             if (towns.getTowns().empty())
@@ -79,7 +109,7 @@ void menu()
             std::chrono::steady_clock::time_point start =
                 std::chrono::steady_clock::now();
             BruteForce bf(towns.getTowns());
-            bf.bruteForce();
+            bf.startBF();
             std::chrono::steady_clock::time_point end =
                 std::chrono::steady_clock::now();
             bf.printRoute();
